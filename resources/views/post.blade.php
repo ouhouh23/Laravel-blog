e2db072ad147cb5e8d358a29c6e029f0fc559899
ouhouh23 <dexter32103210@gmail.com>
2023-03-22 14:32:23 +0300

    init project

---
+++ resources/views/post.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="EN-US">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" type="text/css" href="/app.css">
+
+        <title>Blog</title>
+    </head>
+    <body>
+        <h1>Single post description</h1>
+
+        <article>
+            <?= $post; ?>
+
+            <a href="/">
+                Go back
+            </a>
+        </article>
+    </body>
+</html>
